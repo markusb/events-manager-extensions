3ea5da209efa3bbdf4ff43ac38cc4378ce2dad98
Markus Baertschi <markus@markus.org>
2022-07-18 12:12:54 +0200

    enable-translation

--- events-manager-extensions.php
+++ events-manager-extensions.php
@@ -8,21 +8,27 @@
  * Author: Markus Baertschi
 */
 
+function emex_init(){
+    load_plugin_textdomain( 'emex', false, 'events-manager-extensions/languages' );
+}
+add_action('init', 'emex_init');
+
+
 require_once plugin_dir_path(__FILE__) . 'emex-shortcodes.php';
 
 require_once plugin_dir_path(__FILE__) . 'emex-dashboard.php';
-
-// Hook the 'admin_menu' action hook, run the function named 'mfp_Add_My_Admin_Link()'
-#add_action( 'admin_menu', 'emex_Add_My_Admin_Link' );
 
 // Add a new top level menu link to the ACP
 function emex_Add_My_Admin_Link()
 {
       add_menu_page(
-        'Events manager Extensions by Markus', // Title of the page
+        'Events manager Extensions', // Title of the page
         'Events Manager Extentions', // Text to show on the menu link
         'manage_options', // Capability requirement to see the link
-        'includes/emex-first-acp-page.php' // The 'slug' - file to display when clicking the link
+        'emex-admin-page.php' // The 'slug' - file to display when clicking the link
     );
 }
+// Hook the 'admin_menu' action hook, run the function named 'mfp_Add_My_Admin_Link()'
+#add_action( 'admin_menu', 'emex_Add_My_Admin_Link' );
+
 
