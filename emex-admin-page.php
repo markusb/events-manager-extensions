5aef58f098a507cdff7a65255452109d4fad6adf
Markus Baertschi <markus@markus.org>
2022-07-21 09:18:27 +0200

    Attempt an an admin page

--- emex-admin-page.php
+++ emex-admin-page.php
@@ -1,4 +1,13 @@
 <div class="wrap">
-  <h1>Events Manager Extensions admin page</h1>
-  <p>Admin page</p>
+
+<h1>Events Manager Extensions settings</h1>
+
+<form method="post" action="options.php">
+<?  settings_fields( 'emex' ); ?>
+<? do_settings_sections( 'emex' ); ?>
+
+<?php submit_button(); ?>
+
+</form>
 </div>
+
